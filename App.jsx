61cea3afeef29743ac4b7e1e1e8d4b2da4fd12d0
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-04-16 00:33:32 +0530

    timer

---
+++ App.jsx
@@ -0,0 +1,28 @@
+
+import { useState } from 'react'
+import './App.css'
+import { Timer } from './components/timer'
+
+function App() {
+  const [show,setShow]= useState(true)
+
+  return (
+    <div className="App container">
+    
+     <div className='text-start'>
+       <h2 className='m-5'>Count Down   <button className='btn btn-danger' onClick={()=>{
+                setShow(!show)
+            }}>
+                {show ? " Close" :" Showtimer"}
+            </button>
+            </h2>
+            {show ?  <Timer/> : "" }
+       
+          
+        </div>
+     
+    </div>
+  )
+}
+
+export default App;
